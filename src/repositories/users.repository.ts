4f91fe2a65bb77877f0376daf1a2301fed2be55d
Bruno Lindoso <48108317+bslindoso@users.noreply.github.com>
2025-02-09 08:51:28 -0300

    get user by uuid

--- src/repositories/users.repository.ts
+++ src/repositories/users.repository.ts
@@ -5,17 +5,37 @@ class UserRepository {
   async findAllUsers(): Promise<User[]> {
     try {
       const query = `
-                SELECT uuid, username
-                FROM application_user
-            `;
+        SELECT uuid, username
+        FROM application_user
+      `;
 
       const { rows } = await db.query<User>(query);
       return rows || [];
+
+    } catch (error) {
+      console.error('Error executing query:', error);
+      throw new Error('Database query failed');
+    }
+  }
+
+  async findById(uuid: string): Promise<User> {
+    try {
+      const query = `
+      SELECT uuid, username
+      FROM application_user
+      WHERE uuid = $1
+    `;
+      const values = [uuid];
+      const { rows } = await db.query<User>(query, values);
+      const [user] = rows
+      return user;
+
     } catch (error) {
       console.error('Error executing query:', error);
       throw new Error('Database query failed');
     }
   }
 }
+
 
 export default new UserRepository();
